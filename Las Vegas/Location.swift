919eac21bb40759dd4ceba0c9ee45e981b114c41
Chris Archibald <carchi8py@gmail.com>
2015-10-20 22:34:01 -0700

    Created our Code Data Stack

---
+++ Las Vegas/Location.swift
@@ -0,0 +1,16 @@
+//
+//  Location.swift
+//  Las Vegas
+//
+//  Created by Chris Archibald on 10/20/15.
+//  Copyright © 2015 Chris Archibald. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Location)
+
+class Location: NSManagedObject {
+    
+}
