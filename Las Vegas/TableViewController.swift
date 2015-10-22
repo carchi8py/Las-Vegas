61201b6f95f6f6d53703419240abeccca573026f
Chris Archibald <carchi8py@gmail.com>
2015-10-21 22:07:20 -0700

    Some changes.

--- Las Vegas/TableViewController.swift
+++ Las Vegas/TableViewController.swift
@@ -10,4 +10,24 @@ import UIKit
 
 class TableViewController {
     
+    var locations = [Location]()
+    
+    func locationFinder(locationFinder: ViewController, locations: [Location]) {
+        self.locations = locations
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        var data = locations[indexPath.row]
+        
+        cell.textLabel?.text = data.name
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return locations.count
+    }
+    
 }
